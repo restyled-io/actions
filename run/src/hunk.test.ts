9d469f2ebbad73e0d36d43565fcb45a6ccc2d8d3
patrick brisbin <pbrisbin@gmail.com>
2024-11-07 09:24:05 -0500

    Name some hunk-related types

--- run/src/hunk.test.ts
+++ run/src/hunk.test.ts
@@ -1,35 +1,35 @@
 import { Hunks } from "./hunk";
 import * as NE from "./non-empty";
 
-const ln = (x: number) => {
+const hunkLine = (x: number) => {
   return { lineNumber: x };
 };
 
-const ne = (x: number, ...xs: number[]) => {
+const hunk = (x: number, ...xs: number[]) => {
   const ys = xs.map((x) => {
-    return ln(x);
+    return hunkLine(x);
   });
-  return NE.build(ln(x), ...ys);
+  return NE.build(hunkLine(x), ...ys);
 };
 
 const hunks = new Hunks([
-  ln(1),
-  ln(2),
-  ln(3),
-  ln(7),
-  ln(8),
-  ln(12),
-  ln(13),
-  ln(14),
-  ln(15),
+  hunkLine(1),
+  hunkLine(2),
+  hunkLine(3),
+  hunkLine(7),
+  hunkLine(8),
+  hunkLine(12),
+  hunkLine(13),
+  hunkLine(14),
+  hunkLine(15),
 ]);
 
 describe("Hunks", () => {
   describe("get", () => {
     it("gets contiguous hunks of lines by first lineNumber", () => {
-      expect(hunks.get(1)).toEqual(ne(1, 2, 3));
-      expect(hunks.get(7)).toEqual(ne(7, 8));
-      expect(hunks.get(12)).toEqual(ne(12, 13, 14, 15));
+      expect(hunks.get(1)).toEqual(hunk(1, 2, 3));
+      expect(hunks.get(7)).toEqual(hunk(7, 8));
+      expect(hunks.get(12)).toEqual(hunk(12, 13, 14, 15));
     });
 
     it("returns null for non-first-line lookups", () => {
@@ -49,13 +49,11 @@ describe("Hunks", () => {
 
   describe("contain", () => {
     it("returns true if a hunk is wholly contained in any of the hunks", () => {
-      const hunk = ne(2, 3);
-      expect(hunks.contain(hunk)).toBeTruthy();
+      expect(hunks.contain(hunk(2, 3))).toBeTruthy();
     });
 
     it("returns false if a hunk is not contained any of the hunks", () => {
-      const hunk = ne(2, 3, 4);
-      expect(hunks.contain(hunk)).toBeFalsy();
+      expect(hunks.contain(hunk(2, 3, 4))).toBeFalsy();
     });
   });
 });
