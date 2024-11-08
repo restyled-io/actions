a06c23e103dce946de245af5638994c4ffa25238
patrick brisbin <pbrisbin@gmail.com>
2024-11-08 11:48:16 -0500

    Fix unused import

--- run/src/suggestions.ts
+++ run/src/suggestions.ts
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-import * as core from "@actions/core";
 import { parsePatches } from "./patch";
 
 import { Hunks } from "./hunk";
