f02f3ef8d031ec9bc55f35f291797418453b00e1
patrick brisbin <pbrisbin@gmail.com>
2025-01-22 09:37:12 -0500

    Update license headers

--- run/src/inputs.ts
+++ run/src/inputs.ts
@@ -1,4 +1,4 @@
-/* Copyright (C) 2024 Patrick Brisbin
+/* Copyright (C) 2025 Patrick Brisbin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
